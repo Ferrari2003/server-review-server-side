b51d3cdbedbe7f561be88316a8f8691f1ce211b7
Saiful Islam <https://github.com/Ferrari2003>
2022-11-09 16:38:59 +0600

    aded commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const {  query } = require('express');
+const { MongoClient, ServerApiVersion, ObjectId, } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,13 +24,21 @@ async function run(){
             const cursor = barberCollection.find(query).limit(limit)
             const service = await cursor.toArray()
             res.send(service)
-        })
+        });
         app.get('/services', async(req, res) => {
             const query ={}
             const cursor = barberCollection.find(query)
             const services = await cursor.toArray()
             res.send(services)
-        })
+        });
+
+       app.get('/services/:id', async(req, res) => {
+        const id = req.params.id
+        const query = {_id: ObjectId(id)}
+        const user =await barberCollection.findOne(query)
+        res.send(user)
+       })
+
     }
     finally{
 
