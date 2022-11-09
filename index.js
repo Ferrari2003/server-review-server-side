cf2b149096ba675485a90d37e80aaa922337b038
Saiful Islam <https://github.com/Ferrari2003>
2022-11-10 05:09:18 +0600

    aded commit

--- index.js
+++ index.js
@@ -42,6 +42,17 @@ async function run() {
         })
 
         // review API
+        app.get('/review', async(req, res) => {          
+            let query = {};
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const review = await cursor.toArray();
+            res.send(review);
+        })
         app.post('/review', async(req, res)=> {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
